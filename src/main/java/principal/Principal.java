9d090f0466f7efa216f67ab06db3cada2e7fe079
Vincenzzo <Meu Computador@DESKTOP-4HVJEIT>
2024-05-20 14:24:55 -0300

    - Adicionado Gerenciamento de ALunos

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import visao.FrmMenuPrincipal;
+
+public class Principal {
+    public static void main(String[] args){
+        FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+        // Instancia a interface gráfica
+        objetotela.setVisible(true);
+        // Torna janela visível
+    }
+}
